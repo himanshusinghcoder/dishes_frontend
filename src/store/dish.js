d395b79d8f81e7c4c70c906c48fd222dc04bdf04
himanshu <104294694+himanshusinghcoder@users.noreply.github.com>
2022-12-04 14:52:42 +0530

    Update dish.js

--- src/store/dish.js
+++ src/store/dish.js
@@ -15,7 +15,6 @@ export const useDishStore = create((set,get) => ({
   setShowForm : (value) => set(() => ({showAddForm: value})),
   setAllDishes : async (page = 1) => {
     const data =  await axios.get(`http://localhost:5050/get_all_dishes?page=${page}`)
-    console.log(">>>>", data);
     if(data.data.status === 'success'){
         set(() => ({allDishes: data.data.data.dishes, filterList: data.data.data.dishes, paginationCount: Math.ceil(data.data.data.countDocument/10)}))
     }
