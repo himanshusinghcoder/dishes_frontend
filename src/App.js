a40850c6b4154aab9621d4384d95730122c34e46
himanshu <104294694+himanshusinghcoder@users.noreply.github.com>
2022-12-05 15:23:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Dish from './pages/dish';
 import { Box } from '@mui/material';
 import { useEffect } from 'react';
 import { useDishStore } from './store/dish';
+import { isEmpty } from 'lodash';
 
 function App() {
-    const { setAllDishes }   = useDishStore()
-  useEffect(() => {
-    setAllDishes()
-  }, [])
+    const { setAllDishes,allDishes }   = useDishStore()
+    useEffect(() => {
+      if(isEmpty(allDishes)){
+        setAllDishes()
+      }
+    })
   return (
     <div style={{height: `100vh`, width: `100%`, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
     <Box className='dish_container' component={'div'} sx={{display: 'flex', width: {xs: '100%', md: '70%'}, background: '#eee', height: '80%'}}>
